aa68bc36b36a903298bf02b5d387fb074b7d048d
Marbas Stark <35979271+Edacruz@users.noreply.github.com>
2020-03-07 01:01:26 -0300

    Atualização 0.4

--- maritaka.py
+++ maritaka.py
@@ -1,19 +1,19 @@
 # -*- coding: UTF-8 -*- 
-# Maritaka bot versão 0.3.1
+# Maritaka bot versão 0.3
 import discord
 import asyncio
 import random
 
-versao = '0.3.1'
-build = 'HE 12020/03/03'
+versao = '0.4'
+build = 'HE 12020/03/07'
 client = discord.Client()
-token = 'Discord Tolken'
+token = 'dicord_token'
 
 #imprimindo status
 @client.event
 async def on_ready():
         print('Bot online!')
-        print(client.user.name,'Está online!')
+        print(client.user.name,'está online!')
         print(client.user)
         print(client.user.id)
 
@@ -22,7 +22,11 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith('?oi') and message.author != client.user:
         channel = message.channel
-        await client.send_message(message.channel,'Saudações, {}'.format(message.author))
+        await client.send_message(message.channel,'Saudações, <@{}>'.format(message.author.id))
+    if message.content.startswith('?irineu') and message.author != client.user:
+        channel = message.channel
+        nome = [message.author]
+        await client.send_message(message.channel,'{}, não sabe nem eu\n{}'.format(message.author,nome))
 
 
     if message.content.startswith('?sobre') and message.author != client.user:
@@ -35,17 +39,13 @@ async def on_message(message):
         await client.send_message(message.channel,'Essa deve ser eu kiki :blush:')
 
 
-    if 'Feliz aniversario' in message.content.lower() and message.author != client.user:
-        await client.send_message(message.channel,'Feliz aniversário! 🎈🎉')
-
-
     if 'melhor waifu' in message.content.lower():
         await client.send_message(message.channel,'Essa deve ser eu')
 
 
     if message.content.startswith('?marbas') and message.author != client.user:
         channel = message.channel
-        await client.send_message(message.channel,'O Marbas é o meu criador.')
+        await client.send_message(message.channel,'Lorde Marbas é o meu criador, ele gosta de vacas \'-\'')
 
 
     if message.content.startswith('?sub') and message.author != client.user:
@@ -60,33 +60,47 @@ async def on_message(message):
 
     if message.content.startswith('?chauri') and message.author != client.user:
         channel = message.channel
-        await client.send_message(message.channel,'É a webesposa do lorde Marbas, e também uma deusa entre os humanos.')
+        await client.send_message(message.channel,'É a webesposa do lorde Marbas, e é também uma deusa.')
 
 
     if message.content.startswith('?maritaka') and message.author != client.user:
         msg = message.content
-        channel = message.channel
-        await client.send_message(message.channel,msg[9:])
+        if len(msg) <= 9:
+            await client.send_message(message.channel,'Digite algo!\nSintax: `?maritaka <mensagem>`')
+        else:
+            channel = message.channel
+            await client.send_message(message.channel,msg[9:])
     
     #se o bot for marcado
     if '660353273659916299' in message.content and message.author != client.user:
-        frases = ['Oi','Sup!','Eae','Chamou, sir?','Ao seu dispor','Sim, esta sou eu','Ay ay, sir!','Tô a fazer chá','Estou a apreciar a beleza do lorde marbas, não enche!']
+        frases = ['Oi','Sup!','Eae','Chamou, sir?','Ao seu dispor','Sim, esta sou eu','Ay ay, sir!','Tô a fazer chá','Estou a apreciar a beleza do lorde marbas, não enche!','dormindo :sleeping:']
         await client.send_message(message.channel,random.choice(frases))
 
 #=-=-==-=-= fim dos eventos de mensagem =-=-==-=-=
 
 #flood
     if message.content.startswith('?flood'):
+        msg = message.content
+        if len(msg) < 8:
+            await client.send_message(message.channel,'Digite um valor válido!\nSintax: `?flood <limite>`')
+        elif message.author.id == "user_id":#nega a permissão para um usuário
+            maximo = 0
+            await client.send_message(message.channel,'Você não tem permissão pra usar este comando!')
+        elif int(msg[7:]) > 50:
+            await client.send_message(message.channel,'Limite excedido, digite um valor menor que 50')
+            maximo = 0    
+        else:
+            maximo = int(msg[7:])  
         spam = 0
-        while True:
+        while spam < maximo:
             spam += 1
             msg = message.content
             channel = message.channel
-            await client.send_message(message.channel, spam)
-            if 'pare' in message.content.lower():
-                break  
+            if maximo > 0:
                 await client.send_message(message.channel, spam)
+                
 #fim da tag de flood
+
 
 #=-=-=-=-= código dos joguinhos =-=-=-=-=
 
@@ -98,40 +112,65 @@ async def on_message(message):
         sorteio = (random.randint(0,9),random.randint(0,9),random.randint(0,9))
 
         channel = message.channel
-        await client.send_message(message.channel,sorteio)
 
         #testanto se todos os números são iguais
         if sorteio[0] == sorteio[1] and sorteio[2] == sorteio[0]:
-            await client.send_message(message.channel,'Jackpot! 🎉')
+            await client.send_message(message.channel,'{}\nJackpot! 🎉'.format(sorteio))
+        #se eu perder não serei chamado de otário kkkk
+        elif sorteio[0] != sorteio[1] and message.author.id == "122727645132750848" or sorteio[2] != sorteio[0] and message.author.id == "122727645132750848":
+            await client.send_message(message.channel,'{}\nTu perdeste, lindo :sob:'.format(sorteio))
         else:
-            await client.send_message(message.channel,'Perdeu, otário!') 
+            await client.send_message(message.channel,'{}\nPerdeu, otário! <@{}>'.format(sorteio,message.author.id)) 
 #=-=-=-=-= fim do codigo do jackpot =-=-=-=-=
 
 #=-=-=-=-= joguinho de adivinhar número (a ser implementado) =-=-=-=-=
-#    if message.content.startswith('?joguin') and message.author != client.user:
-#        await client.send_message(message.channel,'Digite um número ')
-#        sua_escolha = message.content
-#        numero = [0,1,2,3,4,5,6,7,8,9]
-#        sekai = random.choice(numero)
-#        if sua_escolha == sekai:
-#            await client.send_message(message.channel,'você ganhou! ')
-#        else:
-#            await client.send_message(message.channel,'você não é tão bom assim é um #fracassado!',sekai)
+    if message.content.startswith('?joguin') and message.author != client.user:
+        msg = message.content
+        if len(msg) <= 7:
+            await client.send_message(message.channel,('Digite algo!\nSintax: `?joguin <numero entre 0 e 9>`'))
+        else:
+            #fatiando a string para pegar apenas o número 
+            sua_escolha = msg[8:]
+            await client.send_message(message.channel,'Você escolheu: **{}**'.format(sua_escolha))
+            numero = ['0','1','2','3','4','5','6','7','8','9']
+            sekai = random.choice(numero)
+            if sua_escolha == sekai:
+                await client.send_message(message.channel,'Você ganhou :partying_face:')
+            elif sua_escolha not in ['0','1','2','3','4','5','6','7','8','9']:
+                await client.send_message(message.channel,'Digite um número entre 0 e 9 :expressionless:')
+            else:
+                await client.send_message(message.channel,'\nResposta certa: **{}**\nVocê perdeu :sob: '.format(sekai))
 #
 #
 #=-=-=-=-= fim joguinho de adivinhar número =-=-=-=-=
+
+#-=-=-=-= Jogo de cara ou coroa -=-=-=-=-=-=-=-=
+    if message.content.startswith('?moeda'):
+        msg = message.content
+        moeda = random.randint(0,1)
+        if moeda == 1:
+            await client.send_message(message.channel,"Cara :wink:")
+        else:
+            await client.send_message(message.channel,"Coroa :crown:") 
+#=-=-=-=-=-= fim dos joguinhos -=-=-=-=-=-=-=-=-=-=
                 
 #mostra todos os comandos: 
     if message.content.startswith('?comandos'):
         msg = message.content
         channel = message.channel
-        await client.send_message(message.channel,'```?marbas\n?tainaka\n?sub\n?maritaka\n?oi\n?chauri\n?flood\n?jackpot```\n*Digite `?help <comando>` para ter detalhes.*')
+        await client.send_message(message.channel,'```?marbas\n?tainaka\n?sub\n?maritaka\n?oi\n?chauri\n?flood\n?jackpot\n?joguin\n?moeda```\n*Digite `?info <comando>` para ter detalhes.*')
+
+#comandos de descrição:
+#    if message.content.startswith('?info joguin'):
+#        channel = message.channel
+#        await client.send_message(message.channel,'Joguinho de adivinhar um número.\nsintax: `joguin <numero entre 0 e 9>`')
+
 
 #versão
     if message.content.startswith('?v'):
         msg = message.content
         channel = message.channel
-        await client.send_message(message.channel,'*Maritaka verão: {}*'.format(versao))
+        await client.send_message(message.channel,'*Maritaka versão: {}*'.format(versao))
    
 
 #rodando o client
